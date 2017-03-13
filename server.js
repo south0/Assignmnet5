6eddf7f3c79f125e63d76ba23ba422067ee0ac0c
Jonny South <joso0757@colorado.edu>
2017-03-12 23:09:07 -0600

    first sub

---
+++ server.js
@@ -0,0 +1,71 @@
+// HTTP PORTION
+
+var http = require('http');
+var fs = require('fs');
+var httpServer = http.createServer(requestHandler);
+var url = require('url');
+httpServer.listen(8080);
+
+function requestHandler(req, res) {
+
+	var parsedUrl = url.parse(req.url);
+	console.log("The Request is: " + parsedUrl.pathname);
+		
+	fs.readFile(__dirname + parsedUrl.pathname, 
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading ' + parsedUrl.pathname);
+			}
+			res.writeHead(200);
+			res.end(data);
+  		}
+  	);
+  	
+}
+
+
+// WEBSOCKET PORTION
+
+var io = require('socket.io').listen(httpServer);
+
+io.sockets.on('connection', 
+
+	function (socket) {
+	
+		console.log("We have a new client: " + socket.id);
+
+
+		
+		///MY SOCKET EVENTS HERE
+
+	
+
+		socket.on('acc', function(data){
+			
+			
+			socket.broadcast.emit('accel', {accx: data.accx});
+
+			})
+		socket.on('accb', function(data){
+			
+			
+			socket.broadcast.emit('accelb', {accxb: data.accxb});
+
+			})
+
+		socket.on('accc', function(data){
+			
+			
+			socket.broadcast.emit('accelc', {accxc: data.accxc});
+
+			})
+
+
+
+
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected " + socket.id);
+		});
+	}
+);
